faa20e3407c77083bab4655d0cd8de42de8a75cc
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2019-12-16 17:19:38 +0100

    add same print output to the reduction script

--- reduce.py
+++ reduce.py
@@ -4,12 +4,21 @@ import ROOT
 ROOT.gROOT.SetBatch(True)
 
 
-def main(path, fraction):
+def main(path, fraction, output):
+    print("Input file: {}".format(path))
     df = ROOT.RDataFrame("Events", path)
+
     count = df.Count()
     num_events = count.GetValue()
+    print("Number of events: {}".format(num_events))
+
+    reduced_events = int(num_events * fraction)
+    print("Reduce the initial dataset to {} events".format(reduced_events))
+
     basename = os.path.basename(path)
-    df.Range(int(num_events * fraction)).Snapshot("Events", os.path.join(output, basename))
+    fullpath = os.path.join(output, basename)
+    print("Write out to {}".format(fullpath))
+    df.Range(reduced_events).Snapshot("Events", fullpath)
 
 
 if __name__ == "__main__":
