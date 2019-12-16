6818a2e3d458cae30f18a9c76d4d63f0141be8b0
Stefan Wunsch <stefan.wunsch@cern.ch>
2019-12-16 17:13:21 +0100

    add reduction script

---
+++ reduce.py
@@ -0,0 +1,24 @@
+import argparse
+import os
+import ROOT
+ROOT.gROOT.SetBatch(True)
+
+
+def main(path, fraction):
+    df = ROOT.RDataFrame("Events", path)
+    count = df.Count()
+    num_events = count.GetValue()
+    basename = os.path.basename(path)
+    df.Range(int(num_events * fraction)).Snapshot("Events", os.path.join(output, basename))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str,
+            help="Full path to NanoAOD file such as given to TFile::Open or the RDataFrame constructor")
+    parser.add_argument("fraction", type=float,
+            help="Fraction of events to be kept, e.g., fraction=0.1 keeps 10% of the original file")
+    parser.add_argument("output", type=str,
+            help="Output directory for the reduced NanoAOD file, the basename of the input file is kept.")
+    args = parser.parse_args()
+    main(args.path, args.fraction, args.output)
---
+++ reduce_nanoaod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASEPATH="root://eospublic.cern.ch//eos/opendata/cms/derived-data/AOD2NanoAODOutreachTool/"
+
+SAMPLES=("GluGluToHToTauTau"
+         "VBF_HToTauTau"
+         "DYJetsToLL"
+         "TTbar"
+         "W1JetsToLNu"
+         "W2JetsToLNu"
+         "W3JetsToLNu"
+         "Run2012B_TauPlusX"
+         "Run2012C_TauPlusX")
+
+for SAMPLE in ${SAMPLES[@]}
+do
+    FULLPATH=${BASEPATH}${SAMPLE}.root
+    python reduce.py $FULLPATH 0.1 $PWD
+done
